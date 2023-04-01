079cfb331d1fb8524bae32db962031b06dc8067a
yingmu <wawamu2201@gmail.com>
2023-04-01 23:28:56 +0200

    top background

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-
 import Sidenav from './components/Sidenav'
+import Main from './components/Main'
 
 function App() {
   
@@ -8,6 +8,7 @@ function App() {
   return (
     <div>
       <Sidenav />
+      <Main />
      
     </div>
   )
