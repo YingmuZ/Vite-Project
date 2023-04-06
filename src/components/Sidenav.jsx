166d211fd19d4b077b507471d369404209e31248
yingmu <wawamu2201@gmail.com>
2023-04-06 18:23:04 +0200

    modified partially

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -37,6 +37,7 @@ const Sidenav = () => {
             <GrProjects size={20} />
             <span className="pl-4">Work</span>
           </a>
+          
           <a onClick={handleNav}
             href="#projects"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
@@ -44,6 +45,7 @@ const Sidenav = () => {
             <AiOutlineProject size={20} />
             <span className="pl-4">Projects</span>
           </a>
+
           <a onClick={handleNav}
             href="#main"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
@@ -51,6 +53,7 @@ const Sidenav = () => {
             <BsPerson size={20} />
             <span className="pl-4">Resume</span>
           </a>
+
           <a onClick={handleNav}
             href="#contact"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
