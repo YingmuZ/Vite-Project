135b63d3570d0d65e4f21527d1c7b1277a69331c
yingmu <wawamu2201@gmail.com>
2023-04-13 22:33:43 +0200

    adjust

--- src/components/SkillItem.jsx
+++ src/components/SkillItem.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 
-const SkillItem = ({year, title, details}) => {
+const SkillItem = ({ title, details}) => {
   return (
-    <ol className='flex flex-col md:flex-row relative border-l border-stone-200'>
-        <li className='mb-10 ml-4'>
-            <div className='absolute w-3 h-3 bg-stone-200 rounded-full mt-1.5 -left-1.5 border-white' />
+    <ol className=' flex flex-col md:flex-row relative border-l border-stone-100'>
+        <li className=' mb-10 ml-4'>
+            <div className='absolute w-3 h-3 bg-stone-600 rounded-full mt-1.5 -left-1.5 border-white' />
             <p className='flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm'>
                 {/* <span className='inline-block px-2 py-1 font-semibold text-white bg-black rounded-md'>{year}</span> */}
                 <span className='text-lg font-semibold text-black'>{title}</span>
                
             </p>
-            <p className='my-2 text-base font-normal text-stone-500'>{details}</p>
+            <p className='my-2 text-base font-normal text-black'>{details}</p>
         </li>
     </ol>
+  
   )
 }
 
