edbe6981f6ded539a08c30481241d513d512bf36
yingmu <wawamu2201@gmail.com>
2023-12-26 23:55:37 +0100

    update info and style adjustment

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,21 @@ import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import bgImg from "./assets/bg.png";
 import AboutMe from "./components/AboutMe";
-import imgMe from "./assets/me.png";
+import imgMe from "./assets/profile.jpg";
+import inset1 from "./assets/inset.jpg";
+import inset2 from "./assets/inset1.jpg";
+import inset3 from "./assets/inset2.jpg";
 import imgCV from "./assets/CV.png";
 import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div>
+    <div className="bg-white/80">
       <Sidenav img={imgCV} />
       <Main img={bgImg} />
 
       <AboutMe img={imgMe} />
-      <Skills img={imgMe} />
+      <Skills img={imgMe} inset1={inset1} inset2={inset2} inset3={inset3} />
       <Projects />
 
       <Contact />
